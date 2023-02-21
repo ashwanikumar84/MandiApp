59f4d423ef61eab679b3af254848bab7033872ff
ashish <min2bhandari@gmail.com>
2023-02-21 21:12:45 +0000

    working on network layer

--- feature-selling/src/main/java/com/jiva/mandi/feature/selling/SellingScreen.kt
+++ feature-selling/src/main/java/com/jiva/mandi/feature/selling/SellingScreen.kt
@@ -1,5 +1,6 @@
 package com.jiva.mandi.feature.selling
 
+import android.widget.Toast
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -8,6 +9,6 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 internal fun SellingButton() {
     Button(onClick = { /*TODO*/ }) {
-        
+//        Toast.makeText(, "hello",Toast.LENGTH_LONG).show()
     }
 }
